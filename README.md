8c5c45d103d0d18ba2565cacf8b2784e5c3573b4
LimHyunKyu <croa@blocko.io>
2020-03-24 17:51:23 +0900

    index.js modify -> get,post,port blablabla

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 Cloning Youtube with Vanilla and NodeJS
 
 git studuy
+
+package.json파일의 scripts 안에 start를 넣음으로써 node index.js 를 치지않고 npm start 만으로도 localhost:4000에 접속하도록 만들었다.
+
+POST request가 정보를 서버로 전달
--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-console.log("hi");
+const express = require("express");
+const app = express();
+
+// respond with "hello world" when a GET request is made to the homepage
+/* app.get("/", function(req, res) {
+  res.send("hello world");
+}); */
+
+const PORT = 4000;
+/* const handleListening = () =>{
+
+} */
+
+const handleListening = function(){
+    console.log(`Listening on: http://localhost:${PORT}`);
+}
+
+const handleHome = function(req,res){
+    console.log(req);
+    res.send('Hello from home');
+}
+
+const handleProfile = function(req,res){
+    res.send("You are on my profile");
+}
+
+app.get("/",handleHome);
+
+app.get("/profile",handleProfile);
+
+app.listen(PORT, handleListening);
