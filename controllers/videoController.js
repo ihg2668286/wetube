362de11660b8e029454db9765516abf433a779b9
LimHyunKyu <croa@blocko.io>
2020-03-26 10:50:34 +0900

    videoController.js, userController.js -> Title updata

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -5,10 +5,12 @@ export const upload = (req, res) => res.send("Upload");
 export const videoDetail = (req, res) => res.send("Video Detail");
 export const editVideo = (req, res) => res.send("Edit Video");
 export const deleteVideo = (req, res) => res.send("Delete Video"); */
-export const home = (req, res) => res.render("home");
-export const search = (req, res) => res.render("search");
-export const videos = (req, res) => res.render("videos");
-export const upload = (req, res) => res.render("upload");
-export const videoDetail = (req, res) => res.render("video Detail");
-export const editVideo = (req, res) => res.render("edit Video");
-export const deleteVideo = (req, res) => res.render("delete Video");
+
+// render함수의 첫번째 인자는 템플릿, 두번째 인자는 템플릿에 추가할 정보가 담긴 객체
+export const home = (req, res) => res.render("home", { pageTitle: "Home" });
+export const search = (req, res) => res.render("search", { pageTitle: "Search" });
+export const videos = (req, res) => res.render("videos", { pageTitle: "Videos" });
+export const upload = (req, res) => res.render("upload", { pageTitle: "Upload" });
+export const videoDetail = (req, res) => res.render("video Detail", { pageTitle: "Video Detail" });
+export const editVideo = (req, res) => res.render("edit Video", { pageTitle: "Edit Video" });
+export const deleteVideo = (req, res) => res.render("delete Video", { pageTitle: "Delete Video" });
